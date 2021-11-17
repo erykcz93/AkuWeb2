1cc0e017d253e770fa0b4df76a37c29074bfb23d
erykcz93 <69258055+erykcz93@users.noreply.github.com>
2021-11-17 16:46:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 const headerMenuMobile = document.querySelector(".header__menu-mobile")
+const NavMobileLinks = document.querySelectorAll(".header__menu-mobile-a")
 const burgerBtn = document.querySelector(".burger-btn")
 const footerYear = document.querySelector(".footer__year")
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+const navScroll = document.querySelector(".scroll-nav")
+const nav = document.querySelector(".nav")
+const hire = document.querySelector(".hire")
+
+// NAV MOBILE
+const handleNav = () => {
+  headerMenuMobile.classList.toggle("mobile--active")
+
+  NavMobileLinks.forEach(item => {
+    item.addEventListener("click", () => {
+      headerMenuMobile.classList.remove("mobile--active")
+    })
+  })
+}
+burgerBtn.addEventListener("click", handleNav)
+
+// NAV MOBILE
 
 function handleCurrentYear() {
   const year = new Date().getFullYear()
   footerYear.innerText = year
 }
 handleCurrentYear()
-
-const handleNav = () => {
-  headerMenuMobile.classList.toggle("menu--active")
-}
-
-burgerBtn.addEventListener("click", handleNav)
 
 window.onload = function () {
   if (
@@ -22,17 +35,23 @@ window.onload = function () {
     // Get all elements with 'data-progress' attribute and run the 'AnimateProgress' funcion with each one
     document
       .querySelectorAll(".progress [data-progress]")
-      .forEach((x) => AnimateProgress(x));
+      .forEach(x => AnimateProgress(x))
   }
-};
+}
 
 function AnimateProgress(el) {
   // Get the element that came as parameter and add the class 'animated-progress' on it
-  el.className = "animate-progress";
+  el.className = "animate-progress"
   // Set the attribute 'style' of this element with the custom attribute '--animate-progress' and the value of 'data-progress' as the width value
   el.setAttribute(
     "style",
     `--animate-progress:${el.getAttribute("data-progress")}%;`
-  );
+  )
   // After this the CSS make its magic
 }
+
+const handleButton = () => {
+  scrollToTopBtn.classList.toggle("visible", window.scrollY > 300),
+    nav.classList.toggle("scroll-nav", window.scrollY > 200),
+}
+window.addEventListener("scroll", handleButton)
